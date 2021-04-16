36a4ce44e4ab59f5cf9cc13a173deb208e22acb1
Bill Wang <bill.wang>
2021-04-16 14:10:30 +1000

    multi arch build

--- ci.sh
+++ ci.sh
@@ -3,6 +3,7 @@
 set -x
 
 if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
+  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
   docker buildx build \
     --progress plain \
     --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6 \
