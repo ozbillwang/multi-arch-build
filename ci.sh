43ad16e3334657d817b43103521f0d21d0088ef8
Bill Wang <bill.wang>
2021-04-16 14:00:31 +1000

    multi arch build

--- ci.sh
+++ ci.sh
@@ -5,7 +5,7 @@ set -x
 if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
   docker buildx build \
     --progress plain \
-    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
+    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6 \
     --tag demo \
     --load .
   exit $?
